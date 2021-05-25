9b538c025a0a32136cc13a858d0c90ed0f985d74
7ganam <7ganam@gmail.com>
2021-05-25 09:05:10 +0200

    refactored the podcasts all view

---
+++ src/components/pages/KnowledgeCenterComponent/PodcastsPageComponent/PodcastsComponent/PodcastsComponent.js
@@ -0,0 +1,116 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Container, Row, Col } from 'reactstrap'
+import "./PodcastsComponent.css"
+import { Card, CardImg, CardBody, CardTitle, CardSubtitle, } from 'reactstrap';
+import ReactLoading from 'react-loading';
+import Slider from "react-slick";
+import pod_img from './pod_logo.png'
+const settings = {
+    dots: false,
+    infinite: true,
+    // speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    cssEase: "linear",
+    responsive: [
+        {
+            breakpoint: 900,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 750,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        }
+    ]
+
+};
+
+function PodcastsComponent(props) {
+
+    console.log(`props.podcasts`, props.podcasts)
+    function generate_podcasts_cards() {
+        const podcasts = props.podcasts.map((podcast, index) => {
+            return (
+                <div md={4} style={{ maxWidth: "200px", marginTop: "20px", marginRight: "20px" }} >
+                    <Card style={{ maxWidth: "280px", boxShadow: "rgb(0 0 0 / 10%) 0px 0px 9px 5px", padding: "8px", margin: 'auto' }} >
+                        <CardImg top width="100%" src={!!podcast.image ? podcast.image.url : '/podcast_image_3.png'} alt="Card image cap" />
+                        <CardBody>
+                            <CardTitle tag="h5" style={{ marginTop: '5px' }}>{podcast.season_episode}:</CardTitle>
+                            <CardSubtitle className='card_title_'>
+                                {podcast.title}
+                            </CardSubtitle>
+                            <Link className="nav_link" to={`/ACTIVITIES/PODCASTS/${podcast.id}`}>
+                                <button type="button" class="mt-2 btn btn-warning">
+                                    Visit podcast
+                            <i class="ml-2 fas fa-headphones"></i>
+                                </button>
+                            </Link>
+                        </CardBody>
+                    </Card>
+                </div>
+
+
+
+            )
+
+        })
+        return podcasts;
+    }
+
+
+
+    return (
+        <div style={{ marginTop: '', marginBottom: '' }}>
+
+
+            <Container>
+                <div className="season_header" style={{ marginTop: "50px ", marginBottom: "0px " }} >
+                    <div className="season_header_logo" >
+                        <img className="season_header_logo_img" src={pod_img} alt='P' />
+                    </div>
+                    <div className="season_header_text_container" >
+                        Season <span className='pod_number'>
+                            {props.season}
+                        </span>
+                    </div>
+                </div>
+            </Container>
+
+            { props.podcasts && props.podcasts.length > 0 ?
+                <Container style={{ marginTop: "10px ", marginBottom: "80px " }} >
+                    {/* <Row style={{ display: "flex", justifyContent: "center" }} > */}
+
+                    {/* {generate_podcasts_cards()} */}
+                    <div id="Carousel_section" style={{ width: "90%", margin: 'auto' }}>
+                        <div style={{ width: "100%", maxWidth: "1200px", margin: 'auto' }}>
+                            {/* <h2> CEBC members</h2> */}
+                            <div>
+                                <Slider className="main_slider" {...settings}>
+                                    {generate_podcasts_cards()}
+                                </Slider>
+                            </div>
+                        </div>
+                    </div>
+                    {/* </Row> */}
+                </Container>
+                :
+                <div style={{ width: '80%', height: '400px', display: 'flex', alignItems: 'center', justifyContent: 'center', margin: 'auto' }}>
+                    <ReactLoading type={"spin"} color={"#00D2F9"} width={"10vw"} />
+                </div>
+            }
+        </div>
+    )
+}
+
+export default PodcastsComponent
--- src/components/pages/KnowledgeCenterComponent/PodcastsPageComponent/PodcastsPageComponent.js
+++ src/components/pages/KnowledgeCenterComponent/PodcastsPageComponent/PodcastsPageComponent.js
@@ -4,11 +4,53 @@ import { Container, Row, Col } from 'reactstrap'
 import "./PodcastsPageComponent.css"
 import { Card, CardImg, CardBody, CardTitle, CardSubtitle, } from 'reactstrap';
 import ReactLoading from 'react-loading';
+import PodcastsComponent from './PodcastsComponent/PodcastsComponent';
+
 
 function PodcastsPageComponent(props) {
 
-    // console.log(`props.podcasts`, props.podcasts)    
+
+
+    function order_podcasts(podcasts) {
+        console.log(`podcasts`, podcasts)
+
+        // collect in array of season_objects seasons_array with the form [ {season:x , episodes:[obj , obj2 , ...]} , {season:y , episods:[obj , obj2 , ...]} ]
+        let seasons_array = [];
+        for (let pod of podcasts) {
+            // console.log(`pod`, pod);
+            //check if the season of the podcast has entry in seasons_array if not add an entry
+            if (seasons_array.length == 0) {
+                seasons_array.push({ season: pod.season, episodes: [pod] })
+            }
+            else {
+                let season_already_exist = false
+                for (let season_object of seasons_array) {
+                    if (season_object.season == pod.season) {
+                        season_already_exist = true;
+                        season_object.episodes.push(pod)
+                    }
+                }
+                if (!season_already_exist) {
+                    seasons_array.push({ season: pod.season, episodes: [pod] })
+                }
+            }
+        }
+
+        // order the seasons objects in the array
+        seasons_array.sort((a, b) => (a.season > b.season) ? 1 : -1)
+
+        // order the episodes in each season_object 
+        for (let season_object of seasons_array) {
+            season_object.episodes.sort((a, b) => (a.episode > b.episode) ? 1 : -1)
+        }
+        console.log(`seasons_array`, seasons_array)
+        return seasons_array;
+
+
+    }
+
     function generate_podcasts_cards() {
+
         const podcasts = props.podcasts.map((podcast, index) => {
             return (
                 <Col md={4} style={{ maxWidth: "300px", marginTop: "20px" }} >
@@ -38,28 +80,50 @@ function PodcastsPageComponent(props) {
         return podcasts;
     }
 
+    function generate_seasons_view() {
+        let seasons_array = order_podcasts(props.podcasts);
+        let seasons_array_view = seasons_array.map(season_object =>
+            <PodcastsComponent season={season_object.season} podcasts={season_object['episodes']} />
+        )
+        return seasons_array_view;
+    }
 
 
     return (
         <div style={{ marginTop: '', marginBottom: '' }}>
-            <div id="title_header" style={{ marginTop: "150px ", marginBottom: "100px " }} >
-                <div class="section_header" style={{ marginTop: "200px" }}>
-                    <span class="section_header_inner"> podcast </span>
-                    <div className="section_header_under"></div>
-                </div>
 
-                {/* <div>
-                    <i class="fas fa-headphones" style={{ fontSize: "60px", marginTop: '20px' }}></i>
-                    </div> */}
-
+            <div style={{ width: "100%", height: "900px", position: "absolute", top: "-50px", right: "", overflow: "hidden", zIndex: '0' }}  >
+                <img src="/assets/images/hero.png" alt="" style={{ width: "100%", height: "auto", position: "", }} />
             </div>
-            { props.podcasts && props.podcasts.length > 0 ?
-                <Container style={{ marginTop: "100px ", marginBottom: "100px " }} >
-                    <Row style={{ display: "flex", justifyContent: "center" }} >
 
-                        {generate_podcasts_cards()}
-                    </Row>
-                </Container>
+            <Container>
+
+                <Row className=" justify-content-center">
+                    <div id="title_header" style={{ marginTop: "150px ", marginBottom: "50px ", zIndex: '3' }} >
+                        <div class="section_header" style={{ marginTop: "20px", zIndex: '3' }}>
+                            <span class="section_header_inner"> podcast </span>
+                            <div className="section_header_under"></div>
+                        </div>
+
+                    </div>
+                </Row>
+            </Container>
+
+
+
+            { props.podcasts && props.podcasts.length > 0 ?
+
+                <div>
+                    {generate_seasons_view()}
+
+                </div>
+                // <Container style={{ marginTop: "100px ", marginBottom: "100px " }} >
+
+                //     <Row style={{ display: "flex", justifyContent: "center" }} >
+
+                //         {generate_podcasts_cards()}
+                //     </Row>
+                // </Container>
                 :
                 <div style={{ width: '80%', height: '400px', display: 'flex', alignItems: 'center', justifyContent: 'center', margin: 'auto' }}>
                     <ReactLoading type={"spin"} color={"#00D2F9"} width={"10vw"} />
