2952d9612d93263c9e0a36835020a3e2de292d99
7ganam@gmail.com <7ganam@gmail.com>
2022-01-31 23:53:24 +0200

    addeg google analytics

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import './App.css';
 import MainComponent from "./mainComponent"
 import { LoginContextProvider, LoginContext } from "./contexts/LoginContext"
 import { useContext } from "react";
+import ReactGA from 'react-ga'
+import React, { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    ReactGA.initialize('G-4SWKZ09KLN');
+    // To Report Page View 
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [])
+
+
   return (
     <div className="App">
       <LoginContextProvider>
