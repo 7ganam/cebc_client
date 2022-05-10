b0da1c95ef8fa6b96a96782fad1c3239100110fd
7ganam@gmail.com <7ganam@gmail.com>
2022-05-10 05:07:46 +0200

    added banar

---
+++ src/components/pages/HomePageComponent/BanarComponent/BanarComponent.js
@@ -0,0 +1,44 @@
+import React, {useEffect, useState} from 'react';
+import {Container, Row, Col} from 'reactstrap';
+import './BanarComponent.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactLoading from 'react-loading';
+import {Link} from 'react-router-dom';
+import moment from 'moment';
+import banar from './banar.png';
+
+function s3_map(str) {
+  str = str.replace('cebc2', 'cebc3');
+  str = str.replace('cebc.s3.eu-central', 'cebc3.s3.eu-central');
+  return str;
+}
+
+function BanarComponent(props) {
+  const [SortedEvents, setSortedEvents] = useState([]);
+
+  return (
+    <div style={{marginTop: '10px'}}>
+      <Container className='banar_container'>
+        <>
+          <Row style={{marginTop: '40px'}}>
+            <Col
+              id='main_banar_col'
+              className='banar_col main_banar_col'
+              xs='12'
+              style={{}}>
+              <a className='' href={`https://evinnovationsummit.com/`}>
+                <div id='main_banar_div' className='p-2'>
+                  <div id='main_banar_img_div' style={{width: '100%'}}>
+                    <img id='main_banar_img' src={banar} alt='oval' />
+                  </div>
+                </div>
+              </a>
+            </Col>
+          </Row>
+        </>
+      </Container>
+    </div>
+  );
+}
+
+export default BanarComponent;
--- src/components/pages/HomePageComponent/HomePageComponent.js
+++ src/components/pages/HomePageComponent/HomePageComponent.js
@@ -1,68 +1,75 @@
-import React, { useEffect } from 'react'
+import React, {useEffect} from 'react';
 
-import NewsComponent from "./NewsComponent/NewsComponent"
-import WorkingGroupsComponent from "./WorkingGroupsComponent/WorkingGroupsComponent"
-import "./HomePageComponent.css"
-import VideoComponent from './VideoComponent/VideoComponent'
-import { TwitterTimelineEmbed } from 'react-twitter-embed';
-import MemebersCarouselComponent from "./MemebersCarouselComponent/MemebersCarouselComponent"
-import CallToAction from '../../shared/CallToAction/CallToAction'
-import PodcastsComponent from './PodcastsComponent/PodcastsComponent'
+import NewsComponent from './NewsComponent/NewsComponent';
+import BanarComponent from './BanarComponent/BanarComponent';
+
+import WorkingGroupsComponent from './WorkingGroupsComponent/WorkingGroupsComponent';
+import './HomePageComponent.css';
+import VideoComponent from './VideoComponent/VideoComponent';
+import {TwitterTimelineEmbed} from 'react-twitter-embed';
+import MemebersCarouselComponent from './MemebersCarouselComponent/MemebersCarouselComponent';
+import CallToAction from '../../shared/CallToAction/CallToAction';
+import PodcastsComponent from './PodcastsComponent/PodcastsComponent';
 
 export default function HomePageComponent(props) {
+  // console.log(`latest_events`, props.LoadedPodcasts)
 
+  return (
+    <div style={{overflow: 'hidden'}}>
+      <VideoComponent />
 
-
-    // console.log(`latest_events`, props.LoadedPodcasts)
-
-
-    return (
-        <div style={{ overflow: 'hidden' }}>
-
-            <VideoComponent />
-
-            {props.members.length > 0 &&
-                <div style={{ marginTop: "20px" }}>
-
-                    <MemebersCarouselComponent members={props.members} />
-
-                </div>
-            }
-
-            <div id="news_section" style={{ marginTop: "0px" }}>
-                <NewsComponent latest_news={props.latest_news} latest_events={props.latest_events} />
-            </div>
-
-
-
-            <div id="working_groups_section" style={{ marginTop: "0px", backgroundColor: "#f7f7f7", paddingBottom: "100px " }}>
-                <WorkingGroupsComponent programmes_state={props.programmes_state} type="working_group" />
-            </div>
-
-
-            <PodcastsComponent podcasts={props.LoadedPodcasts} />
-
-            <div class="section_header" style={{ marginTop: "0px" }}>
-                <span class="section_header_inner" style={{ marginTop: "0px" }}>
-                    Social FEEDS
-                </span>
-                <div className="section_header_under"></div>
-            </div>
-
-
-            <div id="twtr_container" style={{ marginTop: "70px", marginBottom: '70px' }}>
-                <TwitterTimelineEmbed
-                    sourceType="profile"
-                    screenName="CEBC2"
-                    options={{ height: 500, width: 500 }}
-                    style={{ height: "500px", width: "400px" }}
-                />
-            </div>
-
-
-
-
+      {props.members.length > 0 && (
+        <div style={{marginTop: '20px'}}>
+          <MemebersCarouselComponent members={props.members} />
         </div>
+      )}
 
-    )
+      <div id='news_section' style={{marginTop: '0px'}}>
+        <BanarComponent
+          latest_news={props.latest_news}
+          latest_events={props.latest_events}
+        />
+      </div>
+
+      <div id='news_section' style={{marginTop: '0px'}}>
+        <NewsComponent
+          latest_news={props.latest_news}
+          latest_events={props.latest_events}
+        />
+      </div>
+
+      <div
+        id='working_groups_section'
+        style={{
+          marginTop: '0px',
+          backgroundColor: '#f7f7f7',
+          paddingBottom: '100px ',
+        }}>
+        <WorkingGroupsComponent
+          programmes_state={props.programmes_state}
+          type='working_group'
+        />
+      </div>
+
+      <PodcastsComponent podcasts={props.LoadedPodcasts} />
+
+      <div class='section_header' style={{marginTop: '0px'}}>
+        <span class='section_header_inner' style={{marginTop: '0px'}}>
+          Social FEEDS
+        </span>
+        <div className='section_header_under'></div>
+      </div>
+
+      <div
+        id='twtr_container'
+        style={{marginTop: '70px', marginBottom: '70px'}}>
+        <TwitterTimelineEmbed
+          sourceType='profile'
+          screenName='CEBC2'
+          options={{height: 500, width: 500}}
+          style={{height: '500px', width: '400px'}}
+        />
+      </div>
+    </div>
+  );
 }
